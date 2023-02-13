415c812730111635ac3d2cc8e10afdb5c0cb0296
zergolf1994 <zergolf1994@gmail.com>
2023-02-13 14:06:48 +0700

    fix -crf 32

--- Controllers/cv.video.js
+++ Controllers/cv.video.js
@@ -211,6 +211,11 @@ module.exports = async (req, res) => {
       let convert = ffmpeg(inputPath);
       convert.output(outPath);
       convert.size(video_size);
+      convert.outputOptions([
+        "-crf 32",
+        "-movflags faststart",
+        "-max_muxing_queue_size 1024",
+      ]);
       convert.on("start", () => {
         console.log("start", slug, quality);
       });
